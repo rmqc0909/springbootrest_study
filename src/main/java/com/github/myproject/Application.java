14bf901e654a81e667eb38b8a990fdaeac3e59d4
谢丹 <rmqc0909@163.com>
2016-12-21 17:10:31 +0800

    禁用命令行参数

--- src/main/java/com/github/myproject/Application.java
+++ src/main/java/com/github/myproject/Application.java
@@ -12,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setAddCommandLineProperties(false);     //禁用命令行参数
+        application.run(args);
+
     }
 }
